3e2b627f1f297c73893a49b0b48cbe13d99f7780
SRC0747 <RjSrc@0747>
2021-11-30 21:17:58 +0530

    Added : Welcome to SprinngApp to print Hello from Bridgelabz.

---
+++ src/main/java/com/bridgelabz/hellofrombridgelabzspringapp/HelloFromBridgelabzSpringappApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellofrombridgelabzspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloFromBridgelabzSpringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloFromBridgelabzSpringappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/hellofrombridgelabzspringapp/HelloFromBridgelabzSpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellofrombridgelabzspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloFromBridgelabzSpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
