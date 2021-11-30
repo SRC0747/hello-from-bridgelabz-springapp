9210ac3f7612dda41c87d746ed335d5665f78d1a
SRC0747 <RjSrc@0747>
2021-11-30 21:22:52 +0530

    Added : Code autocompiled and print Hello from Bridgelabz.

--- src/main/java/com/bridgelabz/hellofrombridgelabzspringapp/HelloFromBridgelabzSpringappApplication.java
+++ src/main/java/com/bridgelabz/hellofrombridgelabzspringapp/HelloFromBridgelabzSpringappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloFromBridgelabzSpringappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello from Bridgelabz");
 		SpringApplication.run(HelloFromBridgelabzSpringappApplication.class, args);
 	}
 
